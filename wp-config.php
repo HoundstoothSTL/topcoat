11881c3b1121b4e823c5660f8c3c8709f07e9d6b
robbennet <rwbennet@gmail.com>
2013-01-13 12:01:44 -0600

    clean up

--- wp-config.php
+++ wp-config.php
@@ -5,11 +5,15 @@
  * @subpackage Bolt 0.1.0
  */
 
-// Holy crap!  Variables in PHP?
-$default_table_prefix = '446FGD353GasngjsGAj_'; // Why not make this ridiculous?
+/* 
+ * Define the various Database environments and some base settings
+ * Basically set these variables for your own setup
+ * The only stuff you need to adjust are the lines BEFORE the "Don't change past this" Line
+*/
+
+$default_table_prefix = '446FGD353GasngjsGAj_';
 $php_memory_limit = '24M';
 
-// Define the various Database environments
 $local_db_name = '';
 $local_db_user = '';
 $local_db_password = '';
@@ -24,15 +28,40 @@ $prod_db_user = '';
 $prod_db_password = '';
 $prod_db_host = '';
 
+$default_theme_name = '';
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * Go here: https://api.wordpress.org/secret-key/1.1/salt
+ * Copy/Paste what's on that page.
+ *
+ */
+
+define('AUTH_KEY',         ',-=u{^=}f_F`LT~Uf3z+FH}VBXm5_Y+o6Cn|GL+$?,MF6+` .Lw>L-n]7iZ|NEXx');
+define('SECURE_AUTH_KEY',  't3z|3>%q5zp-CqegJ<ffO:SIX-K+|oCX<8*/)t j#]xRpOtv-KPDo8rG|`%+!>q+');
+define('LOGGED_IN_KEY',    'y|yIOS F;-OoNDrk&o!|=^]vp/w,r)C%HRWs>*F-+8qc_gUDv-k{?}1GUEF^CykG');
+define('NONCE_KEY',        ']<LQL`+{[5&ye3-G_FM*a3Y{1]KR>+Q|wTuAwbsj@qVaf^~XY;h!lbJzc|6>{4R/');
+define('AUTH_SALT',        'Yk~|A86*/urFm;|>lJbQMDL?^}`lwg:dDfW%Uae&z;- zUnU]:q|k|, oZXf2THW');
+define('SECURE_AUTH_SALT', '(|]Nnl^7llQ3FcmO(I26=^H?H>j=sh q*(QE!0>Le5OVVbKRQ) HE;;j>KEulAJ3');
+define('LOGGED_IN_SALT',   'z#|!E2CsUMVi^cIc/K*~%+eRy<6s^<;k*8!|!&W8fHCrU_Ik1!CM}kV)f^`7):M%');
+define('NONCE_SALT',       'm8DPPs-,Q&Jjsb?2?hx2;Ik03O]69?4JG,#;mnm4<D-%C17?$)OpG9]XB|+;lA-w');
+
+/*
+ * -------------------------------------------------
+ * 		NO NEED TO CHANGE ANYTHING BELOW HERE
+ * -------------------------------------------------
+*/
+
+
 define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
 define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
-
 define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/content');
 define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/content');
 
-define('WP_DEFAULT_THEME', 'bolt');
-
 define('WP_MEMORY_LIMIT', $php_memory_limit);  // Let's up the PHP Memory limit to compensate for various Server Environments
+define('WP_DEFAULT_THEME', $default_theme_name);
 
 // Be sure to drop an empty 'env_local' file in the root of your project
 if ( file_exists( dirname( __FILE__ ) . '/../env_local' ) ) {
@@ -76,24 +105,6 @@ define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         ',-=u{^=}f_F`LT~Uf3z+FH}VBXm5_Y+o6Cn|GL+$?,MF6+` .Lw>L-n]7iZ|NEXx');
-define('SECURE_AUTH_KEY',  't3z|3>%q5zp-CqegJ<ffO:SIX-K+|oCX<8*/)t j#]xRpOtv-KPDo8rG|`%+!>q+');
-define('LOGGED_IN_KEY',    'y|yIOS F;-OoNDrk&o!|=^]vp/w,r)C%HRWs>*F-+8qc_gUDv-k{?}1GUEF^CykG');
-define('NONCE_KEY',        ']<LQL`+{[5&ye3-G_FM*a3Y{1]KR>+Q|wTuAwbsj@qVaf^~XY;h!lbJzc|6>{4R/');
-define('AUTH_SALT',        'Yk~|A86*/urFm;|>lJbQMDL?^}`lwg:dDfW%Uae&z;- zUnU]:q|k|, oZXf2THW');
-define('SECURE_AUTH_SALT', '(|]Nnl^7llQ3FcmO(I26=^H?H>j=sh q*(QE!0>Le5OVVbKRQ) HE;;j>KEulAJ3');
-define('LOGGED_IN_SALT',   'z#|!E2CsUMVi^cIc/K*~%+eRy<6s^<;k*8!|!&W8fHCrU_Ik1!CM}kV)f^`7):M%');
-define('NONCE_SALT',       'm8DPPs-,Q&Jjsb?2?hx2;Ik03O]69?4JG,#;mnm4<D-%C17?$)OpG9]XB|+;lA-w');
 
 /**#@-*/
 
