47308f63e469e1a94309c22950ef06aeeb1def72
robbennet <rwbennet@gmail.com>
2013-01-12 21:06:22 -0600

    updaging wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,31 +1,75 @@
 <?php
 /**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
+ * Custom WP Config
  * @package WordPress
+ * @subpackage Bolt 0.1.0
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+// Holy crap!  Variables in PHP?
+$default_table_prefix = '446FGD353GasngjsGAj_'; // Why not make this ridiculous?
+$php_memory_limit = '24M';
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+// Define the various Database environments
+$local_db_name = '';
+$local_db_user = '';
+$local_db_password = '';
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+$staging_db_name = '';
+$staging_db_user = '';
+$staging_db_password = '';
+$staging_db_host = '';
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+$prod_db_name = '';
+$prod_db_user = '';
+$prod_db_password = '';
+$prod_db_host = '';
+
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/content');
+
+define('WP_DEFAULT_THEME', 'bolt');
+
+define('WP_MEMORY_LIMIT', $php_memory_limit);  // Let's up the PHP Memory limit to compensate for various Server Environments
+
+// Be sure to drop an empty 'env_local' file in the root of your project
+if ( file_exists( dirname( __FILE__ ) . '/../env_local' ) ) {
+
+    // Local Environment
+    define('WP_ENV', 'local');
+    define('WP_DEBUG', false);
+
+    define('DB_NAME', $local_db_name);
+    define('DB_USER', $local_db_user);
+    define('DB_PASSWORD', $local_db_password);
+    define('DB_HOST', 'localhost');
+
+// Be sure to drop an empty 'env_staging' file in the root of your project
+} elseif ( file_exists( dirname( __FILE__ ) . '/../env_staging' ) ) {
+
+	define('WP_ENV', 'staging');
+    define('WP_DEBUG', false);
+	
+	//Production Environment
+	define('DB_NAME', $staging_db_name);
+	define('DB_USER', $staging_db_user);
+	define('DB_PASSWORD', $staging_db_password);
+	define('DB_HOST', $staging_db_host);
+	
+} else {
+
+	define('WP_ENV', 'production');
+    define('WP_DEBUG', false);
+	
+	//Production Environment
+	define('DB_NAME', $prod_db_name);
+	define('DB_USER', $prod_db_user);
+	define('DB_PASSWORD', $prod_db_password);
+	define('DB_HOST', $prod_db_host);
+	
+}
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -42,14 +86,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         ',-=u{^=}f_F`LT~Uf3z+FH}VBXm5_Y+o6Cn|GL+$?,MF6+` .Lw>L-n]7iZ|NEXx');
+define('SECURE_AUTH_KEY',  't3z|3>%q5zp-CqegJ<ffO:SIX-K+|oCX<8*/)t j#]xRpOtv-KPDo8rG|`%+!>q+');
+define('LOGGED_IN_KEY',    'y|yIOS F;-OoNDrk&o!|=^]vp/w,r)C%HRWs>*F-+8qc_gUDv-k{?}1GUEF^CykG');
+define('NONCE_KEY',        ']<LQL`+{[5&ye3-G_FM*a3Y{1]KR>+Q|wTuAwbsj@qVaf^~XY;h!lbJzc|6>{4R/');
+define('AUTH_SALT',        'Yk~|A86*/urFm;|>lJbQMDL?^}`lwg:dDfW%Uae&z;- zUnU]:q|k|, oZXf2THW');
+define('SECURE_AUTH_SALT', '(|]Nnl^7llQ3FcmO(I26=^H?H>j=sh q*(QE!0>Le5OVVbKRQ) HE;;j>KEulAJ3');
+define('LOGGED_IN_SALT',   'z#|!E2CsUMVi^cIc/K*~%+eRy<6s^<;k*8!|!&W8fHCrU_Ik1!CM}kV)f^`7):M%');
+define('NONCE_SALT',       'm8DPPs-,Q&Jjsb?2?hx2;Ik03O]69?4JG,#;mnm4<D-%C17?$)OpG9]XB|+;lA-w');
 
 /**#@-*/
 
@@ -59,7 +103,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = $default_table_prefix;
 
 /**
  * WordPress Localized Language, defaults to English.
